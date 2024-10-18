82bb62f7db1cabe4b61e2c7aa0c90c7359dd4d94
hugoalh <hugoalh@gmail.com>
2024-10-18 16:59:31 +0800

    Update resources

--- mod.test.ts
+++ mod.test.ts
@@ -1,12 +1,11 @@
 import { assertEquals } from "STD/assert/equals";
 import { isStringSingleLine } from "./mod.ts";
-const sample1 = `Wisi sed et at vero eos nostrud volutpat sed stet dignissim sit sanctus in eros.
+Deno.test("False 1", { permissions: "none" }, () => {
+	assertEquals(isStringSingleLine(`Wisi sed et at vero eos nostrud volutpat sed stet dignissim sit sanctus in eros.
 Et laoreet odio sanctus ea.
 Sea in dolores diam tincidunt labore sea stet vero dolor ut est.
 At aliquyam diam facilisis lorem et takimata et volutpat eros erat ipsum velit labore sed ea illum.
-Dolor lorem sed et volutpat exerci gubergren gubergren tempor quis ea eirmod eos ut dolor autem ipsum accumsan.`;
-Deno.test("False 1", { permissions: "none" }, () => {
-	assertEquals(isStringSingleLine(sample1), false);
+Dolor lorem sed et volutpat exerci gubergren gubergren tempor quis ea eirmod eos ut dolor autem ipsum accumsan.`), false);
 });
 Deno.test("True 1", { permissions: "none" }, () => {
 	assertEquals(isStringSingleLine("Hello, world!"), true);
